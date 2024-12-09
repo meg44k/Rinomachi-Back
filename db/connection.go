d3740baf29f742a7eb94f6d85624df311ab84f5a
meg44k <yugomegane@gmail.com>
2024-12-09 14:54:29 +0900

    追加: 履歴において時間を用いるためにparseTimeをtrueへ変更

--- db/connection.go
+++ db/connection.go
@@ -12,7 +12,7 @@ var DB *sql.DB
 
 func InitDB() {
 	var err error
-	dsn := "root@tcp(127.0.0.1:3306)/renomachi"
+	dsn := "root@tcp(127.0.0.1:3306)/renomachi?parseTime=true"
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
